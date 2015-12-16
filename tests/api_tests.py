e141fe3832074d019184821cc0fc5131d72417e6
Alice Xie <alice.xie@autodesk.com>
2015-12-16 12:13:59 +0000

    Add delete test cases

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -26,6 +26,70 @@ class TestAPI(unittest.TestCase):
         session.close()
         # Remove the tables and their data from the database
         Base.metadata.drop_all(engine)
+    
+    def test_get_empty_posts(self):
+        """ Getting posts from an empty database """
+        response = self.client.get("/api/posts",
+            headers=[("Accept", "application/json")]
+        )
+    
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+    
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data, [])
+         
+    def test_unsupported_accept_header(self):
+        response = self.client.get("/api/posts",
+            headers=[("Accept", "application/xml")]
+        )
 
+        self.assertEqual(response.status_code, 406)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data["message"],
+                         "Request must accept application/json data") 
+                  
+    def test_get_post(self):
+        """ Getting a single post from a populated database """
+        postA = models.Post(title="Example Post A", body="Just a test")
+        postB = models.Post(title="Example Post B", body="Still a test")
+
+        session.add_all([postA, postB])
+        session.commit()
+
+        response = self.client.get("/api/posts/{}".format(postB.id))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        post = json.loads(response.data.decode("ascii"))
+        self.assertEqual(post["title"], "Example Post B")
+        self.assertEqual(post["body"], "Still a test")
+
+    def test_get_non_existent_post(self):
+        """ Getting a single post which doesn't exist """
+        response = self.client.get("/api/posts/1")
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data["message"], "Could not find post with id 1")
+
+    def test_delete_post(self):
+        post = models.Post(title="Example for testing delete post", body="Just a test for delete post")
+        session.add(post)
+        session.commit()
+        
+        response = self.client.delete("/api/posts/{}".format(post.id))
+        self.assertEqual(response.status_code, 200)
+        
+     
+    def test_delete_nonexisting_post(self):
+        response = self.client.delete("/api/posts/{}".format(-1))
+        self.assertEqual(response.status_code, 404)
+        
 if __name__ == "__main__":
     unittest.main()
